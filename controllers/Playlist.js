833a7e11a7ad799386635b46fa0864e309ed9faf
ekoemtzo <ekoemtzo@it.auth.gr>
2025-01-02 00:05:06 +0200

    Fixed violations on the /controllers/Playlist.js file and ensured tests pass

--- controllers/Playlist.js
+++ controllers/Playlist.js
@@ -3,7 +3,7 @@
 var utils = require('../utils/writer.js');
 var Playlist = require('../service/PlaylistService');
 
-module.exports.createPlaylist = function createPlaylist (_, res, _, body) {
+module.exports.createPlaylist = function createPlaylist (_req, res, _next, body) {
   Playlist.createPlaylist(body)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -13,7 +13,7 @@ module.exports.createPlaylist = function createPlaylist (_, res, _, body) {
     });
 };
 
-module.exports.deletePlaylist = function deletePlaylist (_, res, _, userId, playlistId) {
+module.exports.deletePlaylist = function deletePlaylist (_req, res, _next, userId, playlistId) {
   Playlist.deletePlaylist(userId, playlistId)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -23,7 +23,7 @@ module.exports.deletePlaylist = function deletePlaylist (_, res, _, userId, play
     });
 };
 
-module.exports.editPlaylist = function editPlaylist (_, res, _, body, userId, playlistId) {
+module.exports.editPlaylist = function editPlaylist (_req, res, _next, body, userId, playlistId) {
   Playlist.editPlaylist(body, userId, playlistId)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -33,7 +33,7 @@ module.exports.editPlaylist = function editPlaylist (_, res, _, body, userId, pl
     });
 };
 
-module.exports.showUserPlaylists = function showUserPlaylists (_, res, _, userId) {
+module.exports.showUserPlaylists = function showUserPlaylists (_req, res, _next, userId) {
   Playlist.showUserPlaylists(userId)
     .then(function (response) {
       utils.writeJson(res, response);
