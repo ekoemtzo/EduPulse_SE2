9dfaefd3bd34a9b584a05cb9be76e76b1c75f6a6
ekoemtzo <ekoemtzo@it.auth.gr>
2025-01-07 16:20:52 +0200

    Added Comments to the files in the /controllers folder

--- controllers/Playlist.js
+++ controllers/Playlist.js
@@ -1,44 +1,59 @@
-'use strict';
+'use strict'; // Enforces strict mode
 
+// Importing the utility module for writing JSON responses
 var utils = require('../utils/writer.js');
+
+// Importing the PlaylistService
 var Playlist = require('../service/PlaylistService');
 
 module.exports.createPlaylist = function createPlaylist (__, res, _ , body) {
+  // Calls the createPlaylist method from PlaylistService, passing the request body.
   Playlist.createPlaylist(body)
     .then(function (response) {
+      // Sends the response back to the client in JSON format on successful operation.
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // Sends an error response back to the client in JSON format if the operation fails.
       utils.writeJson(res, response);
     });
 };
 
 module.exports.deletePlaylist = function deletePlaylist (__, res, _, userId, playlistId) {
+  // Calls the deletePlaylist method from PlaylistService, passing the userId and playlistId.
   Playlist.deletePlaylist(userId, playlistId)
     .then(function (response) {
+      // Sends the response back to the client in JSON format on successful operation.
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // Sends an error response back to the client in JSON format if the operation fails.
       utils.writeJson(res, response);
     });
 };
 
 module.exports.editPlaylist = function editPlaylist (__, res, _, body, userId, playlistId) {
+  // Calls the editPlaylist method from PlaylistService, passing the request body, userId and playlistId.
   Playlist.editPlaylist(body, userId, playlistId)
     .then(function (response) {
+      // Sends the response back to the client in JSON format on successful operation.
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // Sends an error response back to the client in JSON format if the operation fails.
       utils.writeJson(res, response);
     });
 };
 
 module.exports.showUserPlaylists = function showUserPlaylists (__, res, _, userId) { 
+  // Calls the showUserPlaylists method from PlaylistService, passing the userId.
   Playlist.showUserPlaylists(userId)
     .then(function (response) {
+      // Sends the response back to the client in JSON format on successful operation.
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // Sends an error response back to the client in JSON format if the operation fails.
       utils.writeJson(res, response);
     });
 };
--- controllers/User.js
+++ controllers/User.js
@@ -1,11 +1,14 @@
-'use strict';
+'use strict'; // Enforces strict mode
 
+// Importing the utility module for writing JSON responses
 var utils = require('../utils/writer.js');
+// Importing the UserService
 var User = require('../service/UserService');
 
 module.exports.getUser = function getUser(req, res, next, userId) {
-  // Validate userId
+  // Validate userId by checking if the userId is a positive integer.
   if (!Number.isInteger(Number(userId)) || Number(userId) <= 0) {
+    // If userId is invalid, respond with a 400 status and an appropriate error message.
     return utils.writeJson(
       res,
       { message: "'userId' must be a positive integer." },
@@ -13,8 +16,10 @@ module.exports.getUser = function getUser(req, res, next, userId) {
     );
   }
 
+  // Call the getUser method from UserService, passing the userId.
   User.getUser(userId)
     .then(function (response) {
+      // If no user is found, respond with a 404 status and an error message.
       if (!response) {
         return utils.writeJson(
           res,
@@ -22,9 +27,11 @@ module.exports.getUser = function getUser(req, res, next, userId) {
           404
         );
       }
+      // If a user is found, respond with the user data in JSON format.
       utils.writeJson(res, response);
     })
     .catch(function (error) {
+      // If an error occurs during the process, respond with a 500 status and an internal server error message.
       utils.writeJson(res, { error: "Internal Server Error" }, 500);
     });
 };
